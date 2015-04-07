4e88c0a62efa87cbcb6525848d18db6673a3e992
Horacio Casatti <horacio.casatti@mercadolibre.com>
2015-04-07 12:02:01 -0300

    basic token

---
+++ customized-checkout/card-token/javascript/basic-token-callback.js
@@ -0,0 +1,16 @@
+function sdkResponseHandler(status, response) {
+    if (status != 200 && status != 201) {
+        alert("verify filled data");
+    }else{
+       
+        var form = document.querySelector('#pay');
+
+        var card = document.createElement('input');
+        card.setAttribute('name',"card");
+        card.setAttribute('type',"hidden");
+        card.setAttribute('value',response.id);
+        form.appendChild(card);
+        doSubmit=true;
+        form.submit();
+    }
+};
