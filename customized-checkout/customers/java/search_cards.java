061a1f9a2525e693c94446468b2cfb4de3f018d2
Horacio Casatti <horacio.casatti@mercadolibre.com>
2015-04-08 10:08:47 -0300

    search cards

---
+++ customized-checkout/customers/java/search_cards.java
@@ -0,0 +1,16 @@
+import mercadopago.MP;
+import org.codehaus.jettison.json.JSONObject;
+import org.codehaus.jettison.json.JSONArray;
+
+public class MyClass {
+	public static void main (String args[]) {
+		MP mp = new MP ("ACCESS_TOKEN");
+
+		Map<String, Object> filters = new HashMap<String, Object> ();
+		filters.put("email", "your.payer@email");
+
+		JSONObject customer = mp.get ("/v1/customers/search", filters);
+
+		System.out.println(customer.getJSONObject("response").getJSONArray("cards").toString());
+	}
+}
---
+++ customized-checkout/customers/python/search_cards.py
@@ -0,0 +1,11 @@
+import mercadopago
+
+mp = mercadopago.MP("ACCESS_TOKEN")
+
+filters = {
+    "email": "your.payer@email"
+}
+
+customer = mp.get ("/v1/customers/search", filters);
+
+print customer["response"]["cards"]
