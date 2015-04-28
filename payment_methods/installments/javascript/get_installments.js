06c39c429f346bc2a1ad385e764360ad8602cc5d
Horacio Casatti <horacio.casatti@mercadolibre.com>
2015-04-28 11:24:20 -0300

    Update get_installments.js

--- payment_methods/installments/javascript/get_installments.js
+++ payment_methods/installments/javascript/get_installments.js
@@ -1,7 +1,8 @@
 Mercadopago.setPublishableKey("PUBLIC_KEY");
 
 Mercadopago.getInstallments({
-		"payment_method_id":"visa"
+		"payment_method_id":"visa",
+		"amount": 100
 	}, function (status, response){
 		console.log(response);
 	});
