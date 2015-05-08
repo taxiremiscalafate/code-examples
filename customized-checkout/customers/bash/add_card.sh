bb0620c49dffa115e8e07dc103c2d286faada7e8
Sebastian Gun <sebastian.gun@mercadolibre.com>
2015-05-08 18:02:40 -0300

    access_token should be in the queryString, not in the body

--- customized-checkout/customers/bash/add_card.sh
+++ customized-checkout/customers/bash/add_card.sh
@@ -1,5 +1,5 @@
 curl -X POST \
-     -H 'accept: application/json' \
-     -H 'content-type: application/json' \
-     'https://api.mercadopago.com/v1/customers/[CUSTOMER_ID]/cards?access_token=ACCESS_TOKEN' \
-     -d '{"token": "ff8080814c11e237014c1ff593b57b4d"}'
+	-H 'accept: application/json' \
+	-H 'content-type: application/json' \
+	'https://api.mercadopago.com/v1/customers/[CUSTOMER_ID]/cards?access_token=ACCESS_TOKEN' \
+	-d '{"token": "ff8080814c11e237014c1ff593b57b4d"}'
--- customized-checkout/customers/bash/search_customer.sh
+++ customized-checkout/customers/bash/search_customer.sh
@@ -1,5 +1,4 @@
 curl -X GET \
-     -H 'accept: application/json' \
-     'https://api.mercadopago.com/v1/customers/search' \
-     -d 'access_token=ACCESS_TOKEN' \
-     -d 'email=your.payer@email'
+	-H 'accept: application/json' \
+	'https://api.mercadopago.com/v1/customers/search?access_token=ACCESS_TOKEN' \
+	-d 'email=your.payer@email'
