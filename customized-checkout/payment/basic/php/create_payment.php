7bd75204980bfb991b9734b77325803155091b77
Horacio Casatti <horacio.casatti@mercadolibre.com>
2015-06-23 13:09:38 -0300

    Fixed quotes

--- customized-checkout/payment/basic/php/create_payment.php
+++ customized-checkout/payment/basic/php/create_payment.php
@@ -4,13 +4,13 @@ require_once ('mercadopago.php');
 $mp = new MP('ACCESS_TOKEN');
 
 $payment_data = array(
-	"transaction_amount'" => 100,
-	"token'" => "ff8080814c11e237014c1ff593b57b4d",
-	"description'" => "Title of what you are paying for",
-	"installments'" => 1,
-	"payment_method_id'" => "visa",
-	"payer'" => array (
-		"email'" => "test_user_19653727@testuser.com"
+	"transaction_amount" => 100,
+	"token" => "ff8080814c11e237014c1ff593b57b4d",
+	"description" => "Title of what you are paying for",
+	"installments" => 1,
+	"payment_method_id" => "visa",
+	"payer" => array (
+		"email" => "test_user_19653727@testuser.com"
 	)
 );
 
