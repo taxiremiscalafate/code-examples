2999fb3be538cf0067f94cf642c74894fa362149
Sebastian Gun <sebastian.gun@mercadolibre.com>
2015-06-30 16:00:44 -0300

    Fixed typos

--- customized-checkout/payment/others/ticket/php/create_payment.php
+++ customized-checkout/payment/others/ticket/php/create_payment.php
@@ -4,11 +4,11 @@ require_once ('mercadopago.php');
 $mp = new MP('ACCESS_TOKEN');
 
 $payment_data = array(
-	"transaction_amount'" => 100,
-	"description'" => "Title of what you are paying for",
-	"payment_method_id'" => "TICKET_PAYMENT_METHOD_ID",
-	"payer'" => array (
-		"email'" => "test_user_19653727@testuser.com"
+	"transaction_amount" => 100,
+	"description" => "Title of what you are paying for",
+	"payment_method_id" => "TICKET_PAYMENT_METHOD_ID",
+	"payer" => array (
+		"email" => "test_user_19653727@testuser.com"
 	)
 );
 
--- customized-checkout/payment/others/ticket/ruby/create_payment.rb
+++ customized-checkout/payment/others/ticket/ruby/create_payment.rb
@@ -3,11 +3,11 @@ require 'mercadopago.rb'
 $mp = MercadoPago.new('ACCESS_TOKEN')
 
 paymentData = Hash[
-		"transaction_amount'" => 100,
-		"description'" => "Title of what you are paying for",
-		"payment_method_id'" => "TICKET_PAYMENT_METHOD_ID",
-		"payer'" => Array[
-			"email'" => "test_user_19653727@testuser.com"
+		"transaction_amount" => 100,
+		"description" => "Title of what you are paying for",
+		"payment_method_id" => "TICKET_PAYMENT_METHOD_ID",
+		"payer" => Array[
+			"email" => "test_user_19653727@testuser.com"
 		]
 	]
 
