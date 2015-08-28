bc2856b456c8c012dbf8867d40ee8abb5a1903d8
Sebastian Gun <sebastian.gun@mercadolibre.com>
2015-08-28 15:42:29 -0300

    working cURL examples

--- payment_methods/installments/bash/get_installments.sh
+++ payment_methods/installments/bash/get_installments.sh
@@ -1,6 +1,2 @@
-curl -X GET \
--H "accept: application/json" \
-"https://api.mercadopago.com/v1/payment_methods/installments" \
--d "public_key=PUBLIC_KEY" \
--d "amount=100" \
--d "payment_method_id=visa"
+curl -H "accept: application/json" \
+"https://api.mercadopago.com/v1/payment_methods/installments?public_key=PUBLIC_KEY&payment_method_id=visa"
--- payments/v1/get/bash/get_payment.sh
+++ payments/v1/get/bash/get_payment.sh
@@ -1,4 +1,2 @@
-curl -X GET \
--H "accept: application/json" \
-"https://api.mercadopago.com/v1/payments/[ID]" \
--d "access_token=ACCESS_TOKEN"
+curl -H "accept: application/json" \
+"https://api.mercadopago.com/v1/payments/[ID]?access_token=ACCESS_TOKEN"
