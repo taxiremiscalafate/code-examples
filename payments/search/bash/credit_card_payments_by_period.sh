876b6c4d7520c7f4b9118c01391d5a809e782cd6
Sebastian Gun <sebastian.gun@mercadolibre.com>
2015-09-04 16:23:47 -0300

    Now, this examples actually work

--- payments/search/bash/credit_card_payments_by_period.sh
+++ payments/search/bash/credit_card_payments_by_period.sh
@@ -1,6 +1,6 @@
 curl -G -X GET \
 -H "accept: application/json" \
-"https://api.mercadopago.com/collections/search"
+"https://api.mercadopago.com/collections/search" \
 -d "access_token=ACCESS_TOKEN" \
 -d "payment_type=credit_card" \
 -d "operation_type=regular_payment" \
