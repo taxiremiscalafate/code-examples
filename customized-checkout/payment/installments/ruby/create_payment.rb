b5be1c412bd523577894d405af9a508165ce885b
Sebastian Gun <sebastian.gun@mercadolibre.com>
2016-02-29 12:47:21 -0300

    Minor fixes

--- customized-checkout/payment/installments/ruby/create_payment.rb
+++ customized-checkout/payment/installments/ruby/create_payment.rb
@@ -3,11 +3,11 @@ require 'mercadopago.rb'
 $mp = MercadoPago.new('ACCESS_TOKEN')
 
 paymentData = Hash[
-		"transaction_amount'" => 100,
-		"token'" => "ff8080814c11e237014c1ff593b57b4d",
-		"description'" => "Title of what you are paying for",
-		"payer'" => Hash[
-			"email'" => "test_user_19653727@testuser.com"
+		"transaction_amount" => 100,
+		"token" => "ff8080814c11e237014c1ff593b57b4d",
+		"description" => "Title of what you are paying for",
+		"payer" => Hash[
+			"email" => "test_user_19653727@testuser.com"
 		],
 		"installments" => 3,
 		"payment_method_id" => "master",
--- customized-checkout/payment/one-click/ruby/create_payment.rb
+++ customized-checkout/payment/one-click/ruby/create_payment.rb
@@ -3,11 +3,11 @@ require 'mercadopago.rb'
 $mp = MercadoPago.new('ACCESS_TOKEN')
 
 paymentData = Hash[
-		"transaction_amount'" => 100,
-		"token'" => "ff8080814c11e237014c1ff593b57b4d",
-		"description'" => "Title of what you are paying for",
-		"installments'" => 1,
-		"payer'" => Hash[
+		"transaction_amount" => 100,
+		"token" => "ff8080814c11e237014c1ff593b57b4d",
+		"description" => "Title of what you are paying for",
+		"installments" => 1,
+		"payer" => Hash[
 			"id" => "12345678"
 		]
 	]
