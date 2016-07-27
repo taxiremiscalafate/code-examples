6d2d1b8f1fc5077d37758f9be6dc11cc192f31ff
taxiremiscalafate <taxiremiscalafate@users.noreply.github.com>
2016-07-27 17:45:51 -0300

    Update get_payment.java

--- payments/v1/get/java/get_payment.java
+++ payments/v1/get/java/get_payment.java
@@ -3,7 +3,7 @@ import org.codehaus.jettison.json.JSONObject;
 
 public class MyClass {
 	public static void main (String args[]) {
-		MP mp = new MP ("ACCESS_TOKEN");
+		MP mp = new MP (TEST-5404542076898748-072217-ce2b67a1372f4319ec9ec436012700de__LD_LB__-180292097);
 
 		JSONObject payment = mp.get ("/v1/payments/[ID]");
 
