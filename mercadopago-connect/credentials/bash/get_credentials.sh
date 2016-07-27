cc8f509cfa0c03f13f87900e1b18ac51d6f2f057
taxiremiscalafate <taxiremiscalafate@users.noreply.github.com>
2016-07-27 18:25:45 -0300

    Update get_credentials.sh

--- mercadopago-connect/credentials/bash/get_credentials.sh
+++ mercadopago-connect/credentials/bash/get_credentials.sh
@@ -2,7 +2,8 @@ curl -X POST \
      -H 'accept: application/json' \
      -H 'content-type: application/x-www-form-urlencoded' \
      'https://api.mercadopago.com/oauth/token' \
-     -d 'client_secret=ACCESS_TOKEN' \
+     -d 'client_secret=APP_USR-5404542076898748-072217-89eb6a60f7273970e7b0dc47bd1fb5a8__LC_LD__-180292097' \
      -d 'grant_type=authorization_code' \
      -d 'code=AUTHORIZATION_CODE' \
      -d 'redirect_uri=REDIRECT_URI'
+     
