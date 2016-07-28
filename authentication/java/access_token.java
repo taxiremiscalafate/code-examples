97f4ba155bfc86f28b8003a5ea9d38c8211c0605
taxiremiscalafate <taxiremiscalafate@users.noreply.github.com>
2016-07-27 21:45:39 -0300

    Update access_token.java

--- authentication/java/access_token.java
+++ authentication/java/access_token.java
@@ -2,10 +2,11 @@ import com.mercadopago.MP;
 
 public class MyClass {
 	public static void main (String args[]) {
-		MP mp = new MP ("CLIENT_ID", "CLIENT_SECRET");
+		MP mp = new MP ("5404542076898748", "q79miN8S9AStGJQTLNV3PzQiM1GYb5d8");
 
 		String accessToken = mp.getAccessToken();
 
 		System.out.println(accessToken);
 	}
 }
+
